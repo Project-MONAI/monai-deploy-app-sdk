3299c41bc27cbfae5fb957b9ea7a01520c0b2add
Rahul Choudhury <rahulc@nvidia.com>
2021-07-02 07:39:41 -0700

    adding initial files and directories

---
+++ monai/deploy/apps/simple_imaging_app/my_app.py
@@ -0,0 +1,43 @@
+import sys
+sys.path.insert(0,'../../../../')
+from monai.deploy.foundation.application import Application
+from monai.deploy.apps.simple_imaging_app.sobel_operator import SobelOperator
+from monai.deploy.apps.simple_imaging_app.median_operator import MedianOperator
+from monai.deploy.apps.simple_imaging_app.gaussian_operator import GaussianOperator
+from monai.deploy.executors.single_process_executor import SingleProcessExecutor
+from monai.deploy.executors.multi_process_executor import MultiProcessExecutor
+
+
+from skimage import data, io, filters
+
+class MyApp(Application):
+
+    """ This is a very basic application
+    that showcases the MONAI Deploy application framework
+    """
+    def __init__(self):
+        super().__init__()
+        
+    def compose(self):
+        """ This application has three operators
+        Each operator performes has a single input &
+        a single output port. Each operator performs
+        some kind of image processing function
+        """
+        self.sobel_op = SobelOperator()
+        self.median_op = MedianOperator()
+        self.gaussian_op = GaussianOperator()
+        self.link_operators(self.sobel_op, self.median_op)
+        self.link_operators(self.median_op, self.gaussian_op)
+
+
+
+def main():
+    app = MyApp()
+    executor = SingleProcessExecutor(app)
+    executor.execute()
+
+
+if __name__ == "__main__":
+    main()
+
