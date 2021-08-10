c8147cf1388fb9a53a53ddcfd53242cfdc937ff7
Rahul Choudhury <rahulc@nvidia.com>
2021-08-09 22:41:31 -0700

    adding method to compute and set affine transforms in the series instance

--- monai/deploy/operators/dicom_series_to_volume_operator.py
+++ monai/deploy/operators/dicom_series_to_volume_operator.py
@@ -61,6 +61,7 @@ class DICOMSeriesToVolumeOperator(Operator):
         dicom_series = input.get()
         self.prepare_series(dicom_series)
         metadata = self.create_metadata(dicom_series)
+
         voxel_data = self.generate_voxel_data(dicom_series)
         image = self.create_volumetric_image(voxel_data, metadata)
         output.set(image, "image")
@@ -169,6 +170,7 @@ class DICOMSeriesToVolumeOperator(Operator):
                 series._sop_instances[slice_index].distance = distance
                 series._sop_instances[slice_index].first_pixel_on_slice_normal = point
             else:
+                print("going to removing slice ", slice_index)
                 slice_indices_to_be_removed.append(slice_index)
 
 
@@ -188,9 +190,147 @@ class DICOMSeriesToVolumeOperator(Operator):
             depth_pixel_spacing = (p1[0] - p2[0])*(p1[0] - p2[0]) + (p1[1] - p2[1])*(p1[1] - p2[1]) + (p1[2] - p2[2])*(p1[2] - p2[2])
             depth_pixel_spacing = math.sqrt(depth_pixel_spacing)
             series.depth_pixel_spacing = depth_pixel_spacing
+
+        
+        s_1 = series._sop_instances[0]
+        s_n = series._sop_instances[-1]
+        num_slices = len(series._sop_instances)
+        self.compute_affine_transform(s_1, s_n, num_slices, series)
         
 
-    
+    def compute_affine_transform(self, s_1, s_n, n, series):
+
+        """
+        Computes the affine transform for this series. It does it in both DICOM Patient oriented
+        coordinate system as well as the pne preferred by NIFTI standard. Accordingly, the two attributes
+        dicom_affine_transform and nifti_affine_transform are stored in the series instance
+
+        The Image Orientation Patient contains two triplets, [rx ry rz cx cy cz], which encode
+        direction cosines of the row and column of an image slice. The Image Position Patient of the first slice in
+        a volume, [x1 y1 z1], is the x, y, z coordinates of the upper-left corner voxel of the slice. These two
+        parameters define the location of the slice in PCS. To determine the location of a volume, the Image
+        Position Patient of another slice is normally needed. In practice, we tend to use the position of the last
+        slice in a volume, [xn yn zn]. The voxel size within the slice plane, [vr vc], is stored in object Pixel Spacing.
+
+        Args:
+            s_1: first slice in the series
+            s_n: last slice in the series
+            n: number of slices in the series
+            series: an instance of DICOMSeries
+        """
+        m1 = np.arange(1, 17, dtype=float).reshape(4,4)
+        m2 = np.arange(1, 17, dtype=float).reshape(4,4)
+
+        image_orientation_patient = None
+        try:
+            image_orientation_patient_de = s_1[0x0020,0x0037]
+            if image_orientation_patient_de != None :
+                image_orientation_patient = image_orientation_patient_de.value
+        except KeyError:
+            pass
+        rx = image_orientation_patient[0]
+        ry = image_orientation_patient[1]
+        rz = image_orientation_patient[2]
+        cx = image_orientation_patient[3]
+        cy = image_orientation_patient[4]
+        cz = image_orientation_patient[5]
+
+
+        vr = 0.0
+        vc = 0.0
+        try:
+            pixel_spacing_de = s_1[0x0028, 0x0030]
+            if pixel_spacing_de != None :
+                vr= pixel_spacing_de.value[0]
+                vc = pixel_spacing_de.value[1]
+        except KeyError:
+            pass
+
+        x1 = 0.0
+        y1 = 0.0
+        z1 = 0.0
+
+        xn = 0.0
+        yn = 0.0
+        zn = 0.0
+
+        ip1 = None
+        ip2 = None
+        try:
+            ip1_de = s_1[0x0020,0x0032]
+            ipn_de = s_n[0x0020,0x0032]
+            ip1 = ip1_de .value
+            ipn = ipn_de .value
+
+        except KeyError:
+            pass
+
+        x1 = ip1[0]
+        y1 = ip1[1]
+        z1 = ip1[2]
+
+        xn = ipn[0]
+        yn = ipn[1]
+        zn = ipn[2]
+
+        m1[0,0] = rx*vr
+        m1[0,1] = cx*vc
+        m1[0,2] = (xn - x1)/(n-1)
+        m1[0,3] = x1
+
+        m1[1,0] = ry*vr
+        m1[1,1] = cy*vc
+        m1[1,2] = (yn - y1)/(n-1)
+        m1[1,3] = y1
+
+        m1[2,0] = rz*vr
+        m1[2,1] = cz*vc
+        m1[2,2] = (zn - z1)/(n-1)
+        m1[2,3] = z1
+
+        m1[3,0] = 0
+        m1[3,1] = 0
+        m1[3,2] = 0
+        m1[3,3] = 1
+
+
+        series.dicom_affine_transform = m1
+
+
+
+        m2[0,0] = -rx*vr
+        m2[0,1] = -cx*vc
+        m2[0,2] = -(xn - x1)/(n-1)
+        m2[0,3] = -x1
+
+        m2[1,0] = -ry*vr
+        m2[1,1] = -cy*vc
+        m2[1,2] = -(yn - y1)/(n-1)
+        m2[1,3] = -y1
+
+        m2[2,0] = rz*vr
+        m2[2,1] = cz*vc
+        m2[2,2] = (zn - z1)/(n-1)
+        m2[2,3] = z1
+
+        m2[3,0] = 0
+        m2[3,1] = 0
+        m2[3,2] = 0
+        m2[3,3] = 1
+
+        series.nifti_affine_transform = m2
+
+
+
+
+
+
+
+    def compute_nifti_affine_transform(self, s_1, s_n):
+        pass
+
+
+
     def create_metadata(self, series):
         """
         Collects all relevant metadata from the DICOM Series and creates a dictionary
@@ -254,6 +394,16 @@ class DICOMSeriesToVolumeOperator(Operator):
         except AttributeError:
             pass
 
+        try:
+            metadata["dicom_affine_transform"] =  series.dicom_affine_transform
+        except AttributeError:
+            pass
+
+        try:
+            metadata["nifti_affine_transform"] =  series.nifti_affine_transform
+        except AttributeError:
+            pass
+
 
         return metadata
 
@@ -262,7 +412,7 @@ class DICOMSeriesToVolumeOperator(Operator):
 
 def main():
     op = DICOMSeriesToVolumeOperator()
-    # data_path = "/home/rahul/medical-images/mixed-data/"
+    #data_path = "/home/rahul/medical-images/mixed-data/"
     data_path = "/home/rahul/medical-images/lung-ct-2/"
     files = []
     loader = DICOMDataLoaderOperator()
