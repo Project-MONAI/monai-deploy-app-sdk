fac640853858ae86c521d200fe6e4bcb9bfe212d
mmelqin <mingmelvinq@nvidia.com>
2021-08-24 16:07:27 -0700

    Added comments, resource and env for App

--- examples/apps/ai_spleen_seg_app/app.py
+++ examples/apps/ai_spleen_seg_app/app.py
@@ -10,42 +10,60 @@
 # limitations under the License.
 
 import logging
-from monai.deploy.core import Application
+from monai.deploy.core import Application, env, resource
 from monai.deploy.operators.dicom_data_loader_operator import DICOMDataLoaderOperator
 from monai.deploy.operators.dicom_series_selector_operator import DICOMSeriesSelectorOperator
 from monai.deploy.operators.dicom_series_to_volume_operator import DICOMSeriesToVolumeOperator
 from monai.deploy.operators.dicom_seg_writer_operator import DICOMSegmentationWriterOperator
 from spleen_seg_operator import SpleenSegOperator
 
+@resource(cpu=1, gpu=1)
+# pip_packages can be a string that is a path(str) to requirements.txt file or a list of packages.
+# The monai pkg is not required by this class, instead by the included operators.
+@env(pip_packages=["monai == 0.6.0"])
 class AISpleenSegApp(Application):
-    def __init__(self):
-        self._logger = logging.getLogger(self.__class__.__name__)
+    def __init__(self, *args, **kwargs):
+        """Creates an application instance."""
+        self.logger = logging.getLogger("{}.{}".format(__name__, type(self).__name__))
 
-        super().__init__(do_run=False)
-        print(f'Input: {self._context.input_path}, \
-            Output: {self._context.output_path}, \
-            Models: {self._context.model_path}'
+        super().__init__(*args, **kwargs)
+
+        self.logger.debug(f'App Path: {self.path}, \
+            Input: {self.context.input_path}, \
+            Output: {self.context.output_path},\
+            Models: {self.context.model_path}'
         )
 
-    def __call__(self):
-        #Delegate to base class as there is nothing more to add
+    def run(self):
+        # This method calls the base class to run. Can be omitted if simply calling through.
+        self.logger.debug(f'Begin {self.run.__name__}')
         super().run()
+        self.logger.debug(f'End {self.run.__name__}')
 
     def compose(self):
+        """Creates the app specific operators and chain them up in the processing DAG."""
 
+        self.logger.debug(f'Begin {self.compose.__name__}')
+        # Creates the custom operator(s) as well as SDK built-in operator(s).
         study_loader_op = DICOMDataLoaderOperator()
         series_selector_op = DICOMSeriesSelectorOperator()
         series_to_vol_op = DICOMSeriesToVolumeOperator()
-        spleen_seg_op = SpleenSegOperator(testing=False)
+        spleen_seg_op = SpleenSegOperator()  # This is the app specific operator.
         dicom_seg_writer = DICOMSegmentationWriterOperator()
 
+        # Create the processing pipeline DAG, by specifying the upstream and downstream operators, and
+        # ensuring the output from the former matches the input of the latter, in both name and type.
         self.add_flow(study_loader_op, series_selector_op, {"dicom_study_list": "dicom_study_list"})
         self.add_flow(series_selector_op, series_to_vol_op, {"dicom_series": "dicom_series"})
         self.add_flow(series_to_vol_op, spleen_seg_op, {"image": "image"})
+        # Note below the dicom_seg_writer requires two inputs, each coming from a upstream operator.
         self.add_flow(series_selector_op, dicom_seg_writer, {"dicom_series": "dicom_series"})
         self.add_flow(spleen_seg_op, dicom_seg_writer, {"seg_image": "seg_image"})
 
+        self.logger.debug(f'End {self.compose.__name__}')
+
 if __name__ == "__main__":
-    # Creat the app without immediately running it. The base class needs to change default run=False
-    app_instance = AISpleenSegApp()
-    app_instance()
+    # Creates the app and test it standalone.
+    logging.basicConfig(level=logging.DEBUG)
+    app_instance = AISpleenSegApp()  # Optional params' defaults are fine.
+    app_instance.run()
