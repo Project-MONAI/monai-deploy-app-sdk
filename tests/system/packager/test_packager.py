d4b98cafa05d303d15afa22071a3d3679220fb0b
kavink <kavink@nvidia.com>
2021-09-08 17:10:16 -0700

    Modify packager test to utilize dummy example application code
    Signed-off-by: kavink <kavink@nvidia.com>

--- tests/system/packager/test_packager.py
+++ tests/system/packager/test_packager.py
@@ -9,56 +9,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import argparse
-import json
 import os
-import shutil
-import tempfile
+import subprocess
 
-from monai.deploy.packager import util as packager_util
+from monai.deploy.cli.main import main as monai_deploy
 
 
 def test_packager():
-
-    # Create mock inputs
     test_map_tag = "monaitest:latest"
-    test_app = tempfile.mkdtemp(dir=".")
-    test_models_dir = tempfile.mkdtemp(dir=test_app)
-    test_model = tempfile.mkstemp(dir=test_models_dir, suffix=".ts")
-    with open(test_model[1], "w") as test_model_file:
-        test_model_file.write("Foo")
-    test_params_file = tempfile.mkstemp(dir=".")
+    test_app_path_rel = "examples/apps/simple_imaging_app/"
+    test_app_path = os.path.abspath(test_app_path_rel)
+    args = ["monai-deploy", "package", "-t", test_map_tag, test_app_path]
+    monai_deploy(args)
 
-    test_params_values = {}
-    test_params_values["app-name"] = "test_app"
-    test_params_values["app-version"] = "0.0.0"
-    test_params_values["sdk-version"] = "0.0.0"
-    test_params_values["command"] = "/usr/bin/python3 -u /opt/monai/app/main.py"
-    test_params_values["pip-packages"] = []
-    test_params_values["models"] = []
-    test_params_values["resource"] = {"cpu": 1, "gpu": 2, "memory": "4Gi"}
-
-    test_params_string = json.dumps(test_params_values, sort_keys=True, indent=4, separators=(",", ": "))
-
-    with open(test_params_file[1], "w") as tmp_params:
-        tmp_params.write(test_params_string)
-
-    parser = argparse.ArgumentParser()
-    args = parser.parse_args([])
-    args.application = test_app
-    args.tag = test_map_tag
-    args.params = test_params_file[1]
-    args.base = None
-    args.working_dir = None
-    args.input_dir = None
-    args.output_dir = None
-    args.models_dir = None
-    args.timeout = None
-    args.model = None
-    args.version = None
-    args.verbose = False
-
-    packager_util.package_application(args)
-
-    shutil.rmtree(test_app)
-    os.remove(test_params_file[1])
+    # Delete MONAI application package image
+    docker_rmi_cmd= ["docker", "rmi", "-f", test_map_tag]
+    subprocess.Popen(docker_rmi_cmd)
