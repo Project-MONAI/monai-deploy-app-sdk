7ca2b706c3dfdedc0658f7a9bfeab98dc5aba6f9
Piyush Bhatt <31917351+bhatt-piyush@users.noreply.github.com>
2021-09-13 11:02:04 -0700

    change help formatter (#120)
    Signed-off-by: Piyush Bhatt <pibhatt@nvidia.com>

--- monai/deploy/runner/run_command.py
+++ monai/deploy/runner/run_command.py
@@ -10,7 +10,7 @@
 # limitations under the License.
 
 import logging
-from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser, Namespace, _SubParsersAction
+from argparse import HelpFormatter, ArgumentParser, Namespace, _SubParsersAction
 from typing import List
 
 from monai.deploy.runner import runner
@@ -21,7 +21,7 @@ logger = logging.getLogger("app_runner")
 
 def create_run_parser(subparser: _SubParsersAction, command: str, parents: List[ArgumentParser]) -> ArgumentParser:
     parser = subparser.add_parser(
-        command, formatter_class=ArgumentDefaultsHelpFormatter, parents=parents, add_help=False
+        command, formatter_class=HelpFormatter, parents=parents, add_help=False
     )
 
     parser.add_argument("map", metavar="<map-image[:tag]>", help="MAP image name")
@@ -38,7 +38,7 @@ def create_run_parser(subparser: _SubParsersAction, command: str, parents: List[
         dest="quiet",
         action="store_true",
         default=False,
-        help="Suppress the STDOUT and print only STDERR from the application",
+        help="Suppress the STDOUT and print only STDERR from the application (default: False)",
     )
 
     return parser
