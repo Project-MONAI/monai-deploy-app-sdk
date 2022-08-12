e902820c2ac52df0f2770cd69a4e8a03927878bb
Alvin Ihsani <aihsani@nvidia.com>
2022-08-12 19:22:00 -0400

    add example of Nuance PIN integration

---
+++ examples/integrations/nuance_pin/README.md
@@ -0,0 +1,3 @@
+# Running MONAI Apps in Nuance PIN
+
+MONAI Deploy Apps can be deployed as Nuance PIN applications with minimal effort and zero coding.
---
+++ examples/integrations/nuance_pin/app_wrapper.py
@@ -0,0 +1,121 @@
+"""
+Copyright (c) 2021 Nuance Communications, Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+This module exists to isolate logging configuration in a separate name space
+This allows these values to be updated completely independent of any other
+values in this package
+
+This is an example AI Service.
+
+It meets all of the necessary requirements, but performs trivial actions.
+"""
+
+# Copyright 2022 MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import glob
+import os
+from importlib import import_module
+from pathlib import Path
+from typing import List
+
+import pydicom
+from ai_service import AiJobProcessor, AiService, Series
+from result import ResultStatus
+
+
+class MONAIAppWrapper(AiJobProcessor):
+
+    partner_name = os.environ['AI_PARTNER_NAME']
+    service_name = os.environ['AI_SVC_NAME']
+    service_version = os.environ['AI_SVC_VERSION']
+
+    monai_app_module = os.environ['MONAI_APP_CLASSPATH']
+
+    def filter_image(self, image: pydicom.Dataset) -> bool:
+        return True
+
+    def select_series(
+        self, image_series_list: List[Series]
+    ) -> List[Series]:
+        return image_series_list
+
+    @classmethod
+    def initialize_class(cls):
+        script_folder = Path(__file__).expanduser().resolve().absolute().parent
+        cls.model_path = script_folder / os.getenv("AI_MODEL_PATH", "model.ts")
+
+    def process_study(self):
+        self.logger.info("Starting Processing")
+        self.logger.info(
+            f"{len(self.ai_job.prior_studies)} images in prior studies"
+        )
+
+        if not hasattr(MONAIAppWrapper, 'input_path') or self.input_path is None:
+            self.input_path = self.ai_job.image_folder
+
+        if not hasattr(MONAIAppWrapper, 'output_path') or self.output_path is None:
+            self.output_path = self.ai_job.output_folder
+
+        # create the inference app instance to run on this subprocess
+        self.logger.info("Running MONAI App")
+
+        if not hasattr(MONAIAppWrapper, 'monai_app_instance') or self.monai_app_instance is None:
+            monai_app_class_module = self.monai_app_module.rsplit('.')[0]
+            monai_app_class_name = self.monai_app_module.rsplit('.')[1]
+            monai_app_class = getattr(import_module(monai_app_class_module), monai_app_class_name)
+            self.monai_app_instance = monai_app_class(do_run=False)
+
+        print(f"MONAI App Info: {self.monai_app_instance.get_package_info()}")
+        self.monai_app_instance.run(
+            log_level=self.logger.level,
+            input=self.input_path,
+            output=self.output_path,
+            model=self.model_path,
+            workdir=self.ai_job.folder,
+        )
+
+        dcm_files = glob.glob(f"{os.path.sep}".join([f"{self.output_path}", "**", "*.dcm"]), recursive=True)
+
+        for dcm_file in dcm_files:
+            ds = pydicom.dcmread(dcm_file)
+            series_uid = ds[0x0020000D].value
+
+            self.upload_dicom(
+                os.path.join(self.output_path, dcm_file),
+                series_uid=series_uid,
+            )
+
+        self.logger.info("MONAI App complete")
+
+        self.set_transaction_status(reason=ResultStatus.ANALYSIS_COMPLETE)
+
+
+if __name__ == "__main__":
+    AiService(MONAIAppWrapper).start()
