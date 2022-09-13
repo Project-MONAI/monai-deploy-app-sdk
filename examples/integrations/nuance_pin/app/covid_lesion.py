d2e30ccc099b9083241da009971c60deb6d9a945
Alvin Ihsani <aihsani@nvidia.com>
2022-09-13 18:55:36 -0400

    make covid lesion segmentation operator

--- examples/integrations/nuance_pin/app/covid_lesion.py
+++ examples/integrations/nuance_pin/app/covid_lesion.py
@@ -11,21 +11,18 @@
 
 import logging
 
+from app.inference import LungNoduleSegOperator
+
 from monai.deploy.core import Application, resource
-from monai.deploy.core.domain import Image
-from monai.deploy.core.io_type import IOType
 from monai.deploy.operators.dicom_data_loader_operator import DICOMDataLoaderOperator
 from monai.deploy.operators.dicom_seg_writer_operator import DICOMSegmentationWriterOperator
 from monai.deploy.operators.dicom_series_selector_operator import DICOMSeriesSelectorOperator
 from monai.deploy.operators.dicom_series_to_volume_operator import DICOMSeriesToVolumeOperator
-from monai.deploy.operators.monai_bundle_inference_operator import IOMapping, MonaiBundleInferenceOperator
-
-# from monai.deploy.operators.stl_conversion_operator import STLConversionOperator  # import as needed.
 
 
 @resource(cpu=1, gpu=1, memory="7Gi")
 # The monai pkg is not required by this class, instead by the included operators.
-class AISpleenSegApp(Application):
+class CovidLesionSegApp(Application):
     def __init__(self, *args, **kwargs):
         """Creates an application instance."""
         self._logger = logging.getLogger("{}.{}".format(__name__, type(self).__name__))
@@ -46,19 +43,7 @@ class AISpleenSegApp(Application):
         study_loader_op = DICOMDataLoaderOperator()
         series_selector_op = DICOMSeriesSelectorOperator(Sample_Rules_Text)
         series_to_vol_op = DICOMSeriesToVolumeOperator()
-
-        # Create the inference operator that supports MONAI Bundle and automates the inference.
-        # The IOMapping labels match the input and prediction keys in the pre and post processing.
-        # The model_name is optional when the app has only one model.
-        # The bundle_path argument optionally can be set to an accessible bundle file path in the dev
-        # environment, so when the app is packaged into a MAP, the operator can complete the bundle parsing
-        # during init to provide the optional packages info, parsed from the bundle, to the packager
-        # for it to install the packages in the MAP docker image.
-        # Setting output IOType to DISK only works only for leaf operators, not the case in this example.
-        bundle_spleen_seg_op = MonaiBundleInferenceOperator(
-            input_mapping=[IOMapping("image", Image, IOType.IN_MEMORY)],
-            output_mapping=[IOMapping("pred", Image, IOType.IN_MEMORY)],
-        )
+        seg_op = LungNoduleSegOperator()
 
         # Create DICOM Seg writer with segment label name in a string list
         dicom_seg_writer = DICOMSegmentationWriterOperator(seg_labels=["Spleen"])
@@ -69,37 +54,15 @@ class AISpleenSegApp(Application):
         self.add_flow(
             series_selector_op, series_to_vol_op, {"study_selected_series_list": "study_selected_series_list"}
         )
-        self.add_flow(series_to_vol_op, bundle_spleen_seg_op, {"image": "image"})
+        self.add_flow(series_to_vol_op, seg_op, {"image": "image"})
         # Note below the dicom_seg_writer requires two inputs, each coming from a upstream operator.
         self.add_flow(
             series_selector_op, dicom_seg_writer, {"study_selected_series_list": "study_selected_series_list"}
         )
-        self.add_flow(bundle_spleen_seg_op, dicom_seg_writer, {"pred": "seg_image"})
-        # Create the surface mesh STL conversion operator and add it to the app execution flow, if needed, by
-        # uncommenting the following couple lines.
-        # stl_conversion_op = STLConversionOperator(output_file="stl/spleen.stl")
-        # self.add_flow(bundle_spleen_seg_op, stl_conversion_op, {"pred": "image"})
+        self.add_flow(seg_op, dicom_seg_writer, {"pred": "seg_image"})
 
         logging.info(f"End {self.compose.__name__}")
 
-
-# This is a sample series selection rule in JSON, simply selecting CT series.
-# If the study has more than 1 CT series, then all of them will be selected.
-# Please see more detail in DICOMSeriesSelectorOperator.
-Sample_Rules_Text = """
-{
-    "selections": [
-        {
-            "name": "CT Series",
-            "conditions": {
-                "StudyDescription": "(.*?)",
-                "Modality": "(?i)CT",
-                "SeriesDescription": "(.*?)"
-            }
-        }
-    ]
-}
-"""
 
 if __name__ == "__main__":
     # Creates the app and test it standalone. When running is this mode, please note the following:
@@ -110,4 +73,4 @@ if __name__ == "__main__":
     #     monai-deploy exec app.py -i input -m model/model.ts
     #
     logging.basicConfig(level=logging.DEBUG)
-    app_instance = AISpleenSegApp(do_run=True)
+    app_instance = CovidLesionSegApp(do_run=True)
