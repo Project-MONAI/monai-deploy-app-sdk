f6b56a0bf193da3eba50d6374c506f62bdec654f
Ming M Qin <38891913+MMelQin@users.noreply.github.com>
2022-09-13 18:49:58 -0700

    Improve STL operator so the enclosing app obj can run repetitively (#337)
    * Improve STL operator so the enclosing app obj can run repetitively with the new sample code
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * Fixing Flake8 complaints
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * Ignore Flake8 B024: abstract base class, but it has no abstract methods
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * Silence mypy complaint on types
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * Quiet another mypy error.
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * MyPy complaint fix
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    Signed-off-by: Simone Bendazzoli <simben@kth.se>

--- examples/apps/ai_unetr_seg_app/__main__.py
+++ examples/apps/ai_unetr_seg_app/__main__.py
@@ -1,4 +1,55 @@
+import logging
+import shutil
+import traceback
+from pathlib import Path
+from typing import List
+
 from app import AIUnetrSegApp
 
 if __name__ == "__main__":
-    AIUnetrSegApp(do_run=True)
+    logging.basicConfig(level=logging.DEBUG)
+    # This main function is an example to show how a batch of input can be processed.
+    # It assumes that in the app input folder there are a number of subfolders, each
+    # containing a discrete input to be processed. Each discrete payload can have
+    # multiple DICOM instances file, optionally organized in its own folder structure.
+    # The application object is first created, and on its init the model network is
+    # loaded as well as pre and post processing transforms. This app object is then
+    # run multiple times, each time with a single discrete payload.
+
+    app = AIUnetrSegApp(do_run=False)
+
+    # Preserve the application top level input and output folder path, as the path
+    # in the context may change on each run if the I/O arguments are passed in.
+    app_input_path = Path(app.context.input_path)
+    app_output_path = Path(app.context.output_path)
+
+    # Get subfolders in the input path, assume each one contains a discrete payload
+    input_dirs = [path for path in app_input_path.iterdir() if path.is_dir()]
+
+    # Set the output path for each run under the app's output path, and do run
+    work_dirs: List[str] = []  # strings resprenting folder path
+    for idx, dir in enumerate(input_dirs):
+        try:
+            output_path = app_output_path / f"{dir.name}_output"
+            # Note: the work_dir should be mapped to the host drive when used in
+            #       a container for better performance.
+            work_dir = f".unetr_app_workdir{idx}"
+            work_dirs.extend(work_dir)
+
+            logging.info(f"Start processing input in: {dir} with results in: {output_path}")
+
+            # Run app with specific input and output path.
+            # Passing in the input and output do have the side effect of changing
+            # app context. This side effect will likely be eliminated in later releases.
+            app.run(input=dir, output=output_path, workdir=work_dir)
+
+            logging.info(f"Completed processing input in: {dir} with results in: {output_path}")
+        except Exception as ex:
+            logging.error(f"Failed processing input in {dir}, due to: {ex}\n")
+            traceback.print_exc()
+        finally:
+            # Remove the workdir; alternatively do this later, if storage space is not a concern.
+            shutil.rmtree(work_dir, ignore_errors=True)
+
+    # Alternative. Explicitly remove the working dirs at the end of main.
+    # [shutil.rmtree(work_dir, ignore_errors=True) for work_dir in work_dirs]
