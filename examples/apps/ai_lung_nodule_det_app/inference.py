f799a6b534c2ac0db5dfe6301bd8d448e3a213a7
M Q <mingmelvinq@nvidia.com>
2022-10-26 12:44:38 -0700

    Temp: add the Lung Nodule as a native MONAI Deploy App.
    Signed-off-by: M Q <mingmelvinq@nvidia.com>

---
+++ examples/apps/ai_lung_nodule_det_app/inference.py
@@ -0,0 +1,240 @@
+# Copyright 2021 MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import os
+from typing import Dict, List, Optional
+
+import torch
+
+import monai.deploy.core as md
+from monai.apps.detection.networks.retinanet_detector import RetinaNetDetector
+from monai.apps.detection.transforms.dictionary import (
+    AffineBoxToImageCoordinated,
+    AffineBoxToWorldCoordinated,
+    ClipBoxToImaged,
+)
+from monai.apps.detection.utils.anchor_utils import AnchorGeneratorWithAnchorShape
+from monai.deploy.core import ExecutionContext, Image, InputContext, IOType, Operator, OutputContext
+from monai.deploy.core.domain import Domain
+from monai.deploy.operators.monai_seg_inference_operator import InMemImageReader
+from monai.deploy.utils.importutil import optional_import
+from monai.transforms import (
+    AddChanneld,
+    Compose,
+    CopyItemsd,
+    EnsureChannelFirstd,
+    EnsureTyped,
+    LoadImaged,
+    Orientationd,
+    ScaleIntensityRanged,
+    Spacingd,
+    ToDeviced,
+    ToTensord,
+)
+
+sliding_window_inference, _ = optional_import("monai.inferers", name="sliding_window_inference")
+
+
+class DetectionResult(Domain):
+    def __init__(self, box_data, label_data, score_data, metadata: Optional[Dict] = None):
+        super().__init__(metadata)
+        self._box_data = box_data
+        self._label_data = label_data
+        self._score_data = score_data
+
+    @property
+    def realworld_box_data(self):
+        return self._box_data
+
+    @property
+    def box_data(self):
+        return self._box_data
+
+    @property
+    def label_data(self):
+        return self._label_data
+
+    @property
+    def score_data(self):
+        return self._score_data
+
+
+class DetectionResultList(Domain):
+    def __init__(self, detection_list: List[DetectionResult], metadata: Optional[Dict] = None):
+        super().__init__(metadata)
+        self._detection_list = detection_list
+
+    @property
+    def detection_list(self):
+        return self._detection_list
+
+
+@md.input("image", Image, IOType.IN_MEMORY)
+@md.output("detections", DetectionResultList, IOType.IN_MEMORY)
+@md.env(pip_packages=["monai==0.9.0", "torch>=1.10", "numpy>=1.21", "nibabel"])
+class LungNoduleInferenceOperator(Operator):
+    def __init__(self, model_path: str = "model/model.ts", model_name: str = ""):
+
+        self.logger = logging.getLogger("{}.{}".format(__name__, type(self).__name__))
+        super().__init__()
+        self._input_dataset_key = "image"
+        self._input_dataset_orig_key = "image_orig"
+        self._pred_box_regression = "box_regression"
+        self._pred_label = "box_label"
+        self._pred_score = "box_score"
+        self._pred_labels = "labels"
+
+        # preload the model
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self._model_path = os.path.abspath(model_path)
+        self._model_name = model_name
+        self._model = None
+        self._init_completed = False  # wati till the first call to complete init with model, etc.
+
+    def _delayed_init(self, context:ExecutionContext):
+        if not self._init_completed:
+            self.logger.info("Completing the model loading and detector creation.")
+            if context.models:
+                # `context.models.get(model_name)` returns a model instance if exists.
+                # If model_name is not specified and only one model exists, it returns that model.
+                self._model = context.models.get(self._model_name)
+
+                # The model loaded in the context is missing expected network attrs whne infere runs.
+                # So need to get the path of the model in the context, then load it explicitly in this function.
+                self._model_path = self._model.path
+
+            self.logger.info(f"Loading TorchScript model from: {self._model_path}")
+            self._model = torch.jit.load(self._model_path, map_location=self.device)
+
+        self.logger.info("Loading model into RetinaNetDetector")
+        self.returned_layers = [1, 2]
+        self.base_achor_shapes = [[6, 8, 4], [8, 6, 5], [10, 10, 6]]
+        anchor_generator = AnchorGeneratorWithAnchorShape(
+            feature_map_scales=[1, 2, 4],
+            base_anchor_shapes=self.base_achor_shapes,
+        )
+        self.detector = RetinaNetDetector(
+            network=self._model,
+            anchor_generator=anchor_generator,
+        )
+        self.detector.set_box_selector_parameters(
+            score_thresh=0.1,
+            topk_candidates_per_level=1000,
+            nms_thresh=0.22,
+            detections_per_img=100,
+        )
+        self.detector.set_sliding_window_inferer(
+            roi_size=[240, 240, 160],
+            overlap=0.5,
+            sw_batch_size=1,
+            mode="gaussian",
+            device="cpu",
+        )
+        self.detector.eval()
+
+        self._init_completed = True
+
+    def compute(self, op_input: InputContext, op_output: OutputContext, context: ExecutionContext):
+
+        input_image = op_input.get("image")
+        if not input_image:
+            raise ValueError("Input image not found.")
+
+        self._delayed_init(context=context)
+
+        image_reader = InMemImageReader(input_image)
+
+        with torch.no_grad():
+            processed_image = self.pre_process(image_reader)(
+                (
+                    {
+                        self._input_dataset_key: input_image.metadata().get("SeriesInstanceUID", "Img_in_context"),
+                    },
+                    image_reader,
+                )
+            )
+
+            inference_outputs = self.detector(processed_image[self._input_dataset_key], use_inferer=True)
+
+            pred_boxes = []
+            processed_image[self._input_dataset_key] = torch.squeeze(processed_image[self._input_dataset_key], dim=0)
+            for inference_output in inference_outputs:
+                processed_image[self._pred_box_regression] = inference_output[self.detector.target_box_key]
+                processed_image[self._pred_labels] = inference_output[self.detector.target_label_key]
+                processed_image[self._pred_score] = inference_output[self.detector.pred_score_key]
+
+                processed_image = self.post_process()(processed_image)
+                pred_boxes.append(
+                    DetectionResult(
+                        box_data=processed_image[self._pred_box_regression].numpy(),
+                        label_data=processed_image[self._pred_labels].numpy(),
+                        score_data=processed_image[self._pred_score].numpy(),
+                    )
+                )
+
+            op_output.set(DetectionResultList(pred_boxes), "detections")
+
+    def pre_process(self, img_reader) -> Compose:
+        """Composes transforms for preprocessing input before predicting on a model."""
+
+        image_key = self._input_dataset_key
+        orig_image_key = self._input_dataset_orig_key
+        return Compose(
+            [
+                LoadImaged(
+                    keys=image_key,
+                    reader=img_reader,
+                    affine_lps_to_ras=True,
+                ),
+                CopyItemsd(
+                    keys=[image_key, f"{image_key}_meta_dict"],
+                    names=[orig_image_key, f"{orig_image_key}_meta_dict"],
+                ),
+                ToTensord(keys=image_key),
+                ToDeviced(keys=image_key, device=self.device),
+                EnsureChannelFirstd(keys=image_key),
+                Spacingd(keys=image_key, pixdim=(0.703125, 0.703125, 1.25)),
+                Orientationd(
+                    keys=image_key,
+                    axcodes="RAS",
+                ),
+                AddChanneld(keys=image_key),
+                ScaleIntensityRanged(image_key, a_min=-1024.0, a_max=300.0, b_min=0.0, b_max=1.0, clip=True),
+                EnsureTyped(image_key),
+            ],
+            unpack_items=True,
+            map_items=False,
+        )
+
+    def post_process(self) -> Compose:
+        """Composes transforms for postprocessing the prediction results."""
+
+        return Compose(
+            [
+                ClipBoxToImaged(
+                    box_keys=self._pred_box_regression,
+                    box_ref_image_keys=self._input_dataset_key,
+                    label_keys=[self._pred_labels, self._pred_score],
+                    remove_empty=True,
+                ),
+                AffineBoxToWorldCoordinated(
+                    box_keys=self._pred_box_regression,
+                    box_ref_image_keys=self._input_dataset_key,
+                    affine_lps_to_ras=True,
+                ),
+                AffineBoxToImageCoordinated(
+                    box_keys=[self._pred_box_regression],
+                    box_ref_image_keys=self._input_dataset_orig_key,
+                    affine_lps_to_ras=True,
+                ),
+            ]
+        )
---
+++ examples/apps/ai_lung_nodule_det_app/post_inference_ops.py
@@ -0,0 +1,149 @@
+# Copyright 2021 MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import os
+from random import randint
+from typing import Callable, List
+
+import highdicom as hd
+import numpy as np
+from inference import DetectionResult, DetectionResultList
+
+import monai.deploy.core as md
+from monai.deploy.core import DataPath, ExecutionContext, InputContext, IOType, Operator, OutputContext
+from monai.deploy.core.domain.dicom_series_selection import StudySelectedSeries
+
+
+@md.env(pip_packages=["highdicom==0.19.0"])
+@md.input("original_dicom", List[StudySelectedSeries], IOType.IN_MEMORY)
+@md.input("detection_predictions", DetectionResultList, IOType.IN_MEMORY)
+@md.output("gsps_files", DataPath, IOType.DISK)
+class GenerateGSPSOp(Operator):
+    def __init__(self, upload_gsps_fn: Callable, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.logger = logging.getLogger("{}.{}".format(__name__, type(self).__name__))
+        self.upload_gsps = upload_gsps_fn
+
+    def compute(self, op_input: InputContext, op_output: OutputContext, context: ExecutionContext):
+
+        selected_study = op_input.get("original_dicom")[0]  # assuming a single study
+        selected_series = selected_study.selected_series[0]  # assuming a single series
+        detection_result: DetectionResult = op_input.get("detection_predictions").detection_list[0]
+        output_path = op_output.get("gsps_files").path
+
+        slice_coords = list(range(len(selected_series.series.get_sop_instances())))
+
+        series_uid = hd.UID()
+        series_number = randint(1, 100000)
+
+        # One graphic layer to contain all detections
+        layer = hd.pr.GraphicLayer(
+            layer_name="LUNG_NODULES",
+            order=1,
+            description="Lung Nodule Detections",
+        )
+
+        annotations = []
+
+        all_ref_images = [ins.get_native_sop_instance() for ins in selected_series.series.get_sop_instances()]
+        accession = all_ref_images[0].AccessionNumber
+
+        for inst_num, (box_data, box_score) in enumerate(zip(detection_result.box_data, detection_result.score_data)):
+
+            tracking_id = f"{accession}_nodule_{inst_num}"  # site-specific ID
+            tracking_uid = hd.UID()
+
+            polyline = hd.pr.GraphicObject(
+                graphic_type=hd.pr.GraphicTypeValues.POLYLINE,
+                graphic_data=np.array(
+                    [
+                        [box_data[0], box_data[1]],
+                        [box_data[3], box_data[1]],
+                        [box_data[3], box_data[4]],
+                        [box_data[0], box_data[4]],
+                        [box_data[0], box_data[1]],
+                    ]
+                ),  # coordinates of polyline vertices
+                units=hd.pr.AnnotationUnitsValues.PIXEL,  # units for graphic data
+                tracking_id=tracking_id,
+                tracking_uid=tracking_uid,
+            )
+
+            self.logger.info(f"Box: {[box_data[0], box_data[1], box_data[3], box_data[4]]}")
+
+            text = hd.pr.TextObject(
+                text_value=f"{box_score:.2f}",
+                bounding_box=(box_data[0], box_data[1], box_data[3], box_data[4]),  # left, top, right, bottom
+                units=hd.pr.AnnotationUnitsValues.PIXEL,  # units for bounding box
+                tracking_id=tracking_id,
+                tracking_uid=tracking_uid,
+            )
+
+            affected_slice_idx = [
+                idx
+                for idx, slice_coord in enumerate(slice_coords)
+                if slice_coord >= box_data[2] and slice_coord <= box_data[5]
+            ]
+            ref_images = [
+                selected_series.series.get_sop_instances()[idx].get_native_sop_instance() for idx in affected_slice_idx
+            ]
+            self.logger.info(f"Slice: {[box_data[2], box_data[5]]}, Instances: {affected_slice_idx}")
+
+            if not ref_images:
+                self.logger.error("Finding does not correspond to any series SOP instance")
+                continue
+
+            # A GraphicAnnotation may contain multiple text and/or graphic objects
+            # and is rendered over all referenced images
+            annotation = hd.pr.GraphicAnnotation(
+                referenced_images=ref_images,
+                graphic_layer=layer,
+                text_objects=[text],
+                graphic_objects=[polyline],
+            )
+
+            annotations.append(annotation)
+
+        sop_uid = hd.UID()
+        # Assemble the components into a GSPS object
+        gsps = hd.pr.GrayscaleSoftcopyPresentationState(
+            referenced_images=all_ref_images,
+            series_instance_uid=series_uid,
+            series_number=series_number,
+            sop_instance_uid=sop_uid,
+            instance_number=1,
+            manufacturer="MONAI",
+            manufacturer_model_name="lung_nodule_ct_detection",
+            software_versions="v0.2.0",
+            device_serial_number="",
+            content_label="ANNOTATIONS",
+            graphic_layers=[layer],
+            graphic_annotations=annotations,
+            institution_name="MONAI",
+            institutional_department_name="Deploy",
+            voi_lut_transformations=[
+                hd.pr.SoftcopyVOILUTTransformation(
+                    window_center=-550.0,
+                    window_width=1350.0,
+                )
+            ],
+        )
+
+        gsps_filename = os.path.join(output_path, f"{sop_uid}.dcm")
+        gsps.save_as(gsps_filename)
+
+        if self.upload_gsps:
+            self.upload_gsps(
+                file=gsps_filename,
+                document_detail="MONAI Lung Nodule Detection v0.2.0",
+                series_uid=series_uid,
+            )
