21e59fc35b88c29b396f574926872171448010f9
M Q <mingmelvinq@nvidia.com>
2023-04-06 20:41:25 -0700

    Enhanced nii loader operator after testing with a single operator app.
    Signed-off-by: M Q <mingmelvinq@nvidia.com>

--- monai/deploy/operators/nii_data_loader_operator.py
+++ monai/deploy/operators/nii_data_loader_operator.py
@@ -9,11 +9,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+import logging
 from pathlib import Path
 
 import numpy as np
 
-import monai.deploy.core as md
 from monai.deploy.core import ConditionType, Fragment, Operator, OperatorSpec
 from monai.deploy.utils.importutil import optional_import
 
@@ -28,10 +29,10 @@ class NiftiDataLoader(Operator):
     This operator reads a nifti image, extracts the numpy array and forwards it to the next operator
 
     Named input:
-        image_path: Path to the image file, optional, used to override the path set in the object.
+        image_path: Path to the image file, optional. Use it to override the input path set on the object.
 
     Named output:
-        image: A Numpy object in memory. Downstream receiver optional.
+        image: A Numpy array object. Downstream receiver optional.
     """
 
     def __init__(self, fragment: Fragment, *args, input_path: Path, **kwargs) -> None:
@@ -39,8 +40,9 @@ class NiftiDataLoader(Operator):
 
         Args:
             fragment (Fragment): An instance of the Application class which is derived from Fragment.
-            input_path (Path): The Path to read the image file from, overriden by the named input.
+            input_path (Path): The file Path to read from, overridden by valid named input on compute.
         """
+        self._logger = logging.getLogger("{}.{}".format(__name__, type(self).__name__))
         self.input_path = input_path  # Allow to be None, to be overridden when compute is called.
         self.input_name_path = "image_path"
         self.output_name_image = "image"
@@ -53,14 +55,17 @@ class NiftiDataLoader(Operator):
         spec.output(self.output_name_image).condition(ConditionType.NONE)  # Fine for no or not-ready receiver ports.
 
     def compute(self, op_input, op_output, context):
-        input_path = None
-        try:
-            input_path = op_input.receive(self.input_name_path)
-        except Exception:
-            if self.input_path and not self.input_path.is_file():
+        """Performs computation with the provided context."""
+
+        # The named input port is optional, so must check for and validate the data
+        input_path = op_input.receive(self.input_name_path)
+        if not input_path or not Path(input_path).is_file:
+            self._logger.info(f"No or invalid file path from the optional input port: {input_path}")
+            # Try to fall back to use the object attribute if it is valid
+            if self.input_path and self.input_path.is_file():
                 input_path = self.input_path
             else:
-                raise ValueError("No path given to load image from.")
+                raise ValueError(f"No valid file path from input port or obj attribute: {self.input_path}")
 
         image_np = self.convert_and_save(input_path)
         op_output.emit(image_np, self.output_name_image)
@@ -77,7 +82,8 @@ class NiftiDataLoader(Operator):
 
 
 def test():
-    filepath = "/home/mqin/src/monai-deploy-app-sdk/inputs/lung_seg_ct/nii/volume-covid19-A-0001.nii"  #  "/home/gupta/Documents/mni_icbm152_nlin_sym_09a/mni_icbm152_gm_tal_nlin_sym_09a.nii"
+    # Make sure the file path is correct.
+    filepath = Path(__file__).parent.resolve() / "../../../inputs/lung_seg_ct/nii/volume-covid19-A-0001.nii"
     fragment = Fragment()
     nii_operator = NiftiDataLoader(fragment, input_path=filepath)
     _ = nii_operator.convert_and_save(filepath)
