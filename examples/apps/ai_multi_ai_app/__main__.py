708a05e9002a88b0bffd65b104acc6248ed0d7ab
M Q <mingmelvinq@nvidia.com>
2023-05-08 10:46:55 -0700

    Fixed a typo and format
    Signed-off-by: M Q <mingmelvinq@nvidia.com>

--- examples/apps/ai_multi_ai_app/__main__.py
+++ examples/apps/ai_multi_ai_app/__main__.py
@@ -4,7 +4,8 @@ from app import App
 
 from monai.deploy.logger import load_env_log_level
 
-load_env_log_level()
-logging.info(f"Begin {__name__}")
-App.run()
-logging.info(f"End {__name__}")
+if __name__ == "__main__":
+    load_env_log_level()
+    logging.info(f"Begin {__name__}")
+    App().run()
+    logging.info(f"End {__name__}")
