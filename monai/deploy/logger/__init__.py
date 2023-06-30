6a04890ee68c413874733f6a46644c98efdfc086
M Q <mingmelvinq@nvidia.com>
2023-06-30 10:47:36 -0700

    Remove importing a removed function from holoscan logger
    Signed-off-by: M Q <mingmelvinq@nvidia.com>

--- monai/deploy/logger/__init__.py
+++ monai/deploy/logger/__init__.py
@@ -1,7 +1,4 @@
-# Explicitly import load_env_log_level to quiet mypy complaints.
-# The rest could also be explicit, but then reqires updating when new ones are added.
 from holoscan.logger import *
-from holoscan.logger import load_env_log_level
 
-# Can also use explicit list,
-# from holoscan.logger import load_env_log_level
+# Can also use explicit list, e.g.
+# from holoscan.logger import set_log_level
