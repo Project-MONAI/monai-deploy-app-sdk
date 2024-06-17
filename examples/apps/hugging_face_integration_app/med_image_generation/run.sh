a39262443e42667d06aa124d179dc8ced1fdf807
Vikash Gupta <gupta.vikash@mayo.edu>
2024-06-17 10:18:28 -0400

    Added a sample integration for LLM models in huggingface
    Signed-off-by: Vikash Gupta <gupta.vikash@mayo.edu>

---
+++ examples/apps/hugging_face_integration_app/med_image_generation/run.sh
@@ -0,0 +1,2 @@
+
+python app.py --input_prompt "The patient had residual paralysis of the hand after poliomyelitis. It was necessary to stabilize the thumb with reference to the index finger. This was accomplished by placing a graft from the bone bank between the first and second metacarpals. The roentgenogram shows the complete healing of the graft one year later." --output out.jpg
