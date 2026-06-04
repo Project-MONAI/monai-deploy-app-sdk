f7645c300aa70310695191fad0e6670757134133
M Q <mingmelvinq@nvidia.com>
2026-06-04 01:09:10 -0700

    Fix pytype complaints
    Signed-off-by: M Q <mingmelvinq@nvidia.com>

--- monai/deploy/graphs/__init__.py
+++ monai/deploy/graphs/__init__.py
@@ -1,12 +1,22 @@
-try:
-    from holoscan.flow_graphs import (
-        FlowGraphImpl,
-        FragmentFlowGraph,
-        FragmentFlowGraphImpl,
-        OperatorFlowGraph,
-        OperatorFlowGraphImpl,
-    )
+import importlib
 
+try:
+    _flow_graphs = importlib.import_module("holoscan.flow_graphs")
+except ModuleNotFoundError as e:
+    if e.name != "holoscan.flow_graphs":
+        raise
+    _graphs = importlib.import_module("holoscan.graphs")
+    FlowGraph = _graphs.FlowGraph
+    FragmentFlowGraph = _graphs.FragmentFlowGraph
+    OperatorFlowGraph = _graphs.OperatorFlowGraph
+
+    __all__ = ["FlowGraph", "FragmentFlowGraph", "OperatorFlowGraph"]
+else:
+    FlowGraphImpl = _flow_graphs.FlowGraphImpl
+    FragmentFlowGraph = _flow_graphs.FragmentFlowGraph
+    FragmentFlowGraphImpl = _flow_graphs.FragmentFlowGraphImpl
+    OperatorFlowGraph = _flow_graphs.OperatorFlowGraph
+    OperatorFlowGraphImpl = _flow_graphs.OperatorFlowGraphImpl
     FlowGraph = FlowGraphImpl
 
     __all__ = [
@@ -17,9 +27,3 @@ try:
         "OperatorFlowGraph",
         "OperatorFlowGraphImpl",
     ]
-except ModuleNotFoundError as e:
-    if e.name != "holoscan.flow_graphs":
-        raise
-    from holoscan.graphs import FlowGraph, FragmentFlowGraph, OperatorFlowGraph
-
-    __all__ = ["FlowGraph", "FragmentFlowGraph", "OperatorFlowGraph"]
